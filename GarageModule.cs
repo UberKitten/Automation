e97810e39b9bb6bfcceced1548c57c406cdd0323
Michael West <michael@mwe.st>
2015-06-19 01:37:04 -0500

    Add garage module stub

---
+++ GarageModule.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Automation
+{
+    public class GarageModule : NancyModule
+    {
+        public GarageModule()
+        {
+            Get["/garage/status"] = _ =>
+                {
+                    return Negotiate
+                        .WithAllowedMediaRange("application/json")
+                        .WithModel(new 
+                        {
+                            Open = 0
+                        });
+                };
+        }
+    }
+}
