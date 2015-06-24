a147d962f99907a9f573e7d2a46222b26adc3ab3
Michael West <michael@mwe.st>
2015-06-24 03:40:55 -0500

    Add test authentication to GarageModule with dummy user check

--- Modules/GarageModule.cs
+++ Modules/GarageModule.cs
@@ -3,13 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Nancy.Security;
 
 namespace Automation
 {
     public class GarageModule : NancyModule
     {
+
         public GarageModule()
         {
+            this.RequiresAuthentication();
+            this.RequiresClaims(new[] { "Garage" });
+
             Get["/garage/status"] = _ =>
                 {
                     return Negotiate
