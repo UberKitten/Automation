185cce1679f1fc115e7286e6b567547c17f54f62
Michael West <michael@mwe.st>
2015-06-24 04:46:18 -0500

    Beginnings of HVAC CLI app

--- HVAC/Program.cs
+++ HVAC/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,32 @@ namespace HVAC
     {
         static void Main(string[] args)
         {
+            int failures = 0;
+            while (failures <= 5)
+            {
+                var hubConnection = new HubConnection(Properties.Settings.Default.HubConnection);
+                hubConnection.Headers.Add("X-AUTH-TOKEN", Properties.Settings.Default.Token);
+                var proxy = hubConnection.CreateHubProxy(Properties.Settings.Default.HubProxy);
+
+                proxy.On<Automation.Models.Operation>("UpdateOperation", operation =>
+                {
+                    Console.WriteLine("Received operation");
+                });
+
+                hubConnection.Start().ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Console.WriteLine("Error opening connection {0} ", task.Exception.GetBaseException());
+                        failures++;
+                        System.Threading.Thread.Sleep(1000 * (failures ^ 2)); // exponential backoff
+                    }
+                    else
+                    {
+                        Console.WriteLine("Connected");
+                    }
+                }).Wait();
+            }
         }
     }
 }
