902ce4ffbb61d8299e5401b98d9c537742ec397f
Michael West <michael@mwe.st>
2015-06-24 02:45:21 -0500

    WIP Authentication

---
+++ ModuleSecurity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Automation
+{
+    public class ModuleSecurity
+    {
+        public static void RequiresAuthentication()
+        {
+
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,29 @@
+﻿using Nancy.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Automation
+{
+    public class User : IUserIdentity
+    {
+        public string UserName { get; set; }
+
+        private List<String> _Claims;
+        public IEnumerable<string> Claims {
+            get
+            {
+                if (_Claims == null)
+                {
+                    _Claims = new List<string>();
+                }
+                return _Claims;
+            }
+            set
+            {
+                _Claims = new List<string>(value);
+            }
+        }
+    }
+}
