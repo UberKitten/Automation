41756dfb3bb8ec1855f3d327b2ffca9108011103
Michael West <michael@mwe.st>
2015-06-24 04:45:56 -0500

    Flesh out HVAC operation model

--- Hubs/HVAC.cs
+++ Hubs/HVAC.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using Automation.Models;
+using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,48 @@ namespace Automation
 {
     public class HVAC : Hub
     {
-        public void Test()
+
+        public void SendOperation()
         {
-            ;
+            Clients.All.UpdateOperation(new Operation
+            {
+                Downstairs = new OperationZone
+                {
+                    Compressor = new OperationItem
+                    {
+                        Active = true,
+                        Timeout = new TimeSpan(0, 15, 0)
+                    },
+                    Fan = new OperationItem
+                    {
+                        Active = true,
+                        Timeout = new TimeSpan(0, 10, 0)
+                    },
+                    Heat = new OperationItem
+                    {
+                        Active = false,
+                        Timeout = TimeSpan.Zero
+                    }
+                },
+                Upstairs = new OperationZone
+                {
+                    Compressor = new OperationItem
+                    {
+                        Active = true,
+                        Timeout = new TimeSpan(0, 5, 0)
+                    },
+                    Fan = new OperationItem
+                    {
+                        Active = true,
+                        Timeout = new TimeSpan(0, 7, 0)
+                    },
+                    Heat = new OperationItem
+                    {
+                        Active = false,
+                        Timeout = TimeSpan.Zero
+                    }
+                }
+            });
         }
     }
 }
