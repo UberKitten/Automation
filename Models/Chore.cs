7137bf2239d1abbfe5c006929141a5653a4611c6
Michael West <michael@mwe.st>
2015-09-22 01:40:51 -0500

    Add basic Chore models and ChoreRecurrence Enumerable

---
+++ Models/Chore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Automation.Models
+{
+    public class Chore
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ChoreGroup
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Dictionary<Chore, User> Chores { get; set; }
+    }
+}
