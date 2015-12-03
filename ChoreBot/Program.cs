8264832fa610544dab167014b78193cf51a1ff10
UberKitten <566438+UberKitten@users.noreply.github.com>
2015-12-03 01:45:42 -0600

    Add Quartz.NET job scheduling to WebJob

--- ChoreBot/Program.cs
+++ ChoreBot/Program.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Quartz;
+using Quartz.Impl;
 
 namespace ChoreBot
 {
     class Program
     {
-        public static void NagChoreGroup(string choreGroup)
+        static void Main()
         {
-            Automation.Modules.GroupMeModule.NagChoreGroup(choreGroup);
+            var host = new JobHost();
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+
+            // Quartz.NET scheduling
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            var householdJob = JobBuilder.Create<NagChoreGroupJob>()
+                .UsingJobData("ChoreGroup", "Household")
+                .Build();
+
+            var trigger = TriggerBuilder.Create()
+                .StartNow()
+                .Build();
+
+            scheduler.ScheduleJob(householdJob, trigger);
+        }
+
+
+        
+    }
+
+    public class NagChoreGroupJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Automation.Modules.GroupMeModule.NagChoreGroup((string)context.Get("ChoreGroup"));
         }
     }
 }
