d29a6b9929a3ffb1ce8d98f9b993558b58bdad3d
UberKitten <566438+UberKitten@users.noreply.github.com>
2015-12-03 01:29:11 -0600

    Remove continuous WebJob code

--- ChoreBot/Program.cs
+++ ChoreBot/Program.cs
@@ -7,19 +7,8 @@ using Microsoft.Azure.WebJobs;
 
 namespace ChoreBot
 {
-    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
     class Program
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
-        // AzureWebJobsDashboard and AzureWebJobsStorage
-        static void Main()
-        {
-            var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
-            host.RunAndBlock();
-        }
-
-        [NoAutomaticTrigger]
         public static void NagChoreGroup(string choreGroup)
         {
             Automation.Modules.GroupMeModule.NagChoreGroup(choreGroup);
