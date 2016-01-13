17a69453497f873286a7dd3f6dfb84db5d86153f
UberKitten <566438+UberKitten@users.noreply.github.com>
2016-01-13 02:29:41 -0600

    Add /checkauth method

--- Modules/IndexModule.cs
+++ Modules/IndexModule.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Nancy.Security;
 
 namespace Automation.Modules
 {
@@ -11,6 +12,21 @@ namespace Automation.Modules
         public IndexModule()
         {
             Get["/"] = _ => "It works!";
+
+            // Nancy does not like returning an empty model and an empty view (like only a status code)
+            // So we give it a "model" if there's no claim specified using default routing
+            Get["/checkauth/{claim?null}"] = _ =>
+            {
+                this.RequiresAuthentication();
+
+                // This is dumb
+                if (_.claim.Value != "null")
+                {
+                    this.RequiresClaims(new string[] { _.claim.Value});
+                }
+
+                return HttpStatusCode.NoContent;
+            };
         }
     }
 }
