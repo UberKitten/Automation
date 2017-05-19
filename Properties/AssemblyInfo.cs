1cbe2289a2b40604d45d7d3af6f35d8db3890fc2
UberKitten <566438+UberKitten@users.noreply.github.com>
2017-05-19 01:29:25 -0500

    Create new project and transfer code to keep

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Automation")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4ac989de-54c9-40e9-92b0-f60944f1447d")]
+[assembly: Guid("7f105d56-6abf-4bc5-88c6-df07220da452")]
 
 // Version information for an assembly consists of the following four values:
 //
