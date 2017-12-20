33575350d5da3f8806ab9c2caac065820398ed78
UberKitten <566438+UberKitten@users.noreply.github.com>
2017-12-20 03:57:39 -0600

    Populate more fields in old SQL server code

--- AutomationBootstrapper.cs
+++ AutomationBootstrapper.cs
@@ -35,27 +35,42 @@ namespace Automation
                     {
                         sql.Open();
                         var check = sql.CreateCommand();
-                        check.CommandText = @"SELECT [User].UserName, [Claim].Name FROM [Token]
-JOIN [User] on [User].Id = [Token].UserId
+                        check.CommandText = @"SELECT [User].UserName, [User].GroupMeId, [Claim].Name, [ClaimURL].[URL] FROM [Token]
+JOIN [User] on [User].Id = Token.UserId
 LEFT JOIN [UserClaim] on [UserClaim].UserId = [User].Id
 LEFT JOIN [Claim] on [Claim].Id = [UserClaim].ClaimId
+LEFT JOIN [ClaimURL] on [ClaimURL].ClaimId = [Claim].Id
 WHERE [Token].Value = @token";
                         check.Parameters.AddWithValue("token", token);
 
                         var user = new User();
+                        var claims = new HashSet<string>();
+                        var urls = new HashSet<string>();
+
                         using (var reader = check.ExecuteReader())
                         {
                             while (reader.Read())
                             {
                                 user.UserName = reader.GetString(0);
 
-                                // Don't add a NULL claim if user doesn't have any claims
                                 if (!reader.IsDBNull(1))
                                 { 
-                                    ((List<String>)user.Claims).Add(reader.GetString(1));
+                                    user.GroupMeId = reader.GetInt32(1);
+                                }
+
+                                if (!reader.IsDBNull(2))
+                                { 
+                                    claims.Add(reader.GetString(2));
+                                }
+
+                                if (!reader.IsDBNull(3))
+                                {
+                                    urls.Add(reader.GetString(3));
                                 }
                             }
                         }
+                        user.Claims = claims;
+                        user.ClaimURLs = urls;
 
                         if (user.UserName != null)
                         {
